86992c37605d11da23f4e49711113e67c74cdaa2
WadaTakafumi <wdtk2525@gmail.com>
2019-07-10 11:09:08 +0900

    ログイン実装

--- asset/app/controllers/users_controller.rb
+++ asset/app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-
-  
+  before_action :authenticate_log!
 
   # GET /users
   # GET /users.json
--- asset/config/routes.rb
+++ asset/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :logs
   resources :users
+  get 'hello/index' => 'hello#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
